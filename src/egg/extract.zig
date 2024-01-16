1f4da1790da452c53b190680c510f185c9945033
AdamGurArie <adam.gurarie@cryptosat.io>
2024-01-16 18:44:51 +0200

    extraction is workinggit branch

--- src/egg/extract.zig
+++ src/egg/extract.zig
@@ -7,8 +7,11 @@ pub fn extract(comptime L: type) type {
     return struct {
         eclass: egg.Id,
         partialExp: std.ArrayList(L),
+        eclass_stack: std.ArrayList(egg.Id),
 
-        fn eval_eclass_min_cost(self: *@This(), eclass_id: egg.Id, egraph: anytype) !usize {
+        const enode_cost_pair = struct { cost: usize, enode: L };
+
+        fn eval_eclass_min_cost(self: *@This(), eclass_id: egg.Id, egraph: anytype) !enode_cost_pair {
             var enodes = egraph.get(eclass_id).?.*.nodes.items;
             var min_enode = enodes[0];
             var min_cost = enodes[0].getExpressionCost();
@@ -21,60 +24,51 @@ pub fn extract(comptime L: type) type {
             }
 
             try self.partialExp.append(min_enode);
-            return min_cost;
+            return enode_cost_pair{ .cost = min_cost, .enode = min_enode };
         }
 
         fn eval_expression_cost(self: *@This(), eclass_id: egg.Id, egraph: anytype) !usize {
             var total_cost: usize = 0;
-            var eclass = egraph.get(eclass_id);
-            total_cost += try eval_eclass_min_cost(self, eclass_id, egraph);
-            std.log.warn("current_cost: {}", .{total_cost});
-            var children = eclass.?.children.values();
-            std.log.warn("number of childrens: {}", .{children.len});
-            for (children) |child| {
-                total_cost += try eval_eclass_min_cost(self, child, egraph);
-                std.log.warn("current cost: {}", .{total_cost});
+            var ret_pair = try eval_eclass_min_cost(self, eclass_id, egraph);
+            total_cost += ret_pair.cost;
+            var children = ret_pair.enode.getMutableChildren();
+            for (children orelse return total_cost) |child| {
+                try self.eclass_stack.append(child);
+                std.log.warn("push child into stack: {}\n", .{child});
+                //total_cost += try eval_eclass_min_cost(self, child, egraph);
             }
 
-            std.log.warn("total expression cost: {}", .{total_cost});
             return total_cost;
         }
 
         pub fn find_cheapest_expression(self: *@This(), root_eclass: egg.Id, egraph: anytype) !usize {
             var total_cost: usize = 0;
-            var eclass_stack = std.ArrayList(egg.Id).init(std.testing.allocator);
-            defer eclass_stack.deinit();
 
-            try eclass_stack.append(root_eclass);
+            try self.eclass_stack.append(root_eclass);
             while (true) {
-                if (eclass_stack.items.len == 0) {
+                if (self.eclass_stack.items.len == 0) {
                     break;
                 }
 
-                var eclass = eclass_stack.popOrNull() orelse return total_cost; // eclass_stack.items[eclass_stack.items.len - 1];
+                var eclass = self.eclass_stack.popOrNull().?; // eclass_stack.items[eclass_stack.items.len - 1];
                 total_cost += try eval_expression_cost(self, eclass, egraph);
-                std.log.warn("current cost: {}", .{try eval_expression_cost(self, eclass, egraph)});
-                var childrens = egraph.get(eclass).?.children;
-                std.log.warn("childrens: {}", .{childrens.values().len});
-                for (childrens.values()) |children| {
-                    std.log.warn("children: {}", .{egraph.get(children).?.*.nodes.items[0]});
-                    try eclass_stack.append(children);
-                }
-
-                std.log.warn("eclass_stack size before pop: {}", .{eclass_stack.items.len});
-                _ = eclass_stack.popOrNull() orelse return total_cost;
-                std.log.warn("eclass_stack size: {}", .{eclass_stack.items.len});
+                std.log.warn("eclass stack len: {}", .{self.eclass_stack.items.len});
+                // for (childrens.children.values()) |children| {
+                // std.log.warn("children: {}", .{egraph.get(children).?.*.nodes.items[0]});
+                // try eclass_stack.append(children);
+                // }
             }
 
             return total_cost;
         }
 
         pub fn init(eclass: egg.Id) @This() {
-            return @This(){ .eclass = eclass, .partialExp = std.ArrayList(L).init(std.testing.allocator) };
+            return @This(){ .eclass = eclass, .partialExp = std.ArrayList(L).init(std.testing.allocator), .eclass_stack = std.ArrayList(egg.Id).init(std.testing.allocator) };
         }
 
         pub fn deinit(self: *@This()) void {
             self.partialExp.deinit();
+            self.eclass_stack.deinit();
         }
     };
 }
@@ -86,15 +80,11 @@ test "extract" {
     defer egraph.deinit();
 
     var const1 = try egraph.addEclass(.{ .constant = 16 });
+    var const2 = try egraph.addEclass(.{ .constant = 17 });
     // var const2 = try egraph.addEclass(.{ .constant = 18 });
-    _ = try egraph.addEclass(.{ .add = .{ const1, const1 } });
-
-    var childrens = egraph.get(0).?.*.children;
-    var children = egraph.get(childrens.values()[0]);
-    std.log.warn("childrens: {}", .{children.?.children.values().len});
-    std.log.warn("children children {}", .{children.?.children.keys().len});
+    var expr = try egraph.addEclass(.{ .add = .{ const1, const2 } });
     var extract_obj = extract(ToyLanguage).init(0);
-    var cost: usize = try extract_obj.find_cheapest_expression(0, egraph);
+    var cost: usize = try extract_obj.find_cheapest_expression(expr, egraph);
     std.log.warn("cost: {}", .{cost});
     for (extract_obj.partialExp.items) |lang| {
         std.log.warn("obj: {}", .{lang});
