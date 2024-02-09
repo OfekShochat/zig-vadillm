441e40d4c013f15bf3007158ae6b90f337e62dba
AdamGurArie <adam.gurarie@cryptosat.io>
2024-02-09 21:49:15 +0200

    rvsdg

--- src/codegen/ir/rvsdg.zig
+++ src/codegen/ir/rvsdg.zig
@@ -1,51 +1,69 @@
 const Instruction = @import("../instructions.zig").Instruction;
 const std = @import("std");
+const Type = @import("../types.zig").Type;
 
 // intra procedural
 
 const Region = struct {
+    region_parents: []Edge,
     insts: []Node,
+
+    pub fn getParents() []*Region {}
 };
 
 const GamaNode = struct {
     cond: []Instruction,
     cond_eval_map: std.HashMap(u32, u32),
     paths: []Region,
-    exit_node: anyopaque,
+    exit_node: *Node,
+    parents: []Edge,
 };
 
 const ThetaNode = struct {
     tail_cond: []Instruction,
     loop_body: Region,
-    exit_node: anyopaque,
+    exit_node: *Node,
+    parents: []Edge,
 };
 
 // inter procedural
 
 const LambdaNode = struct {
-    arguments: [],
+    arguments: []Type,
     function_body: Region,
-    output: ,
+    exit_node: *Node,
+    parents: []*Edge,
 };
 
 const DeltaNode = struct {
-
-}
+    value: Region,
+    inputs: []*Region,
+    output: *Region,
+    parents: []*Edge,
+};
 
 const PhiNode = struct {
-
-}
+    input: Region,
+    output: Region,
+    parents: []*Edge,
+};
 
 const OmegaNode = struct {
+    region: Region,
+    parents: []*Edge,
+};
 
-}
+const Edge = struct {
+    state: bool,
+    end: *Region,
+};
 
 const Node = union {
     simple: Instruction,
     gama: GamaNode,
     theta: ThetaNode,
     lambda: LambdaNode,
-    delta: DeltaNodes,
+    delta: DeltaNode,
     phi: PhiNode,
     omega: OmegaNode,
 };
