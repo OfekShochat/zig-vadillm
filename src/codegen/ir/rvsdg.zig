16f91f9530aa64b39bbf3091dfc6f1e9268811b0
AdamGurArie <adam.gurarie@cryptosat.io>
2024-02-13 18:56:32 +0200

    change to support egraphs

--- src/codegen/ir/rvsdg.zig
+++ src/codegen/ir/rvsdg.zig
@@ -1,19 +1,19 @@
 const Instruction = @import("../instructions.zig").Instruction;
 const std = @import("std");
 const Type = @import("../types.zig").Type;
+const Id = @import("../egg/egg.zig").Id;
 // intra procedural
 
 const Region = struct {
-    region_parents: []*Region,
+    id: Id,
     insts: []Node,
 };
 
 // Gama node represents an if-else-then statement it consists of condition which is just a region,
 // and the evaluation of this region will affect the chosen case.
 const GamaNode = struct {
-    cond: Region,
-    cases: []Region,
-    outputs: []Region,
+    cond: Id,
+    paths: []Id,
 };
 
 // Theta node represents a tail-controlled loop, i.e do-while,
@@ -21,16 +21,22 @@ const GamaNode = struct {
 // and this result will decide wether to continue or break.
 // the tail condition is just Region.
 const ThetaNode = struct {
-    tail_cond: []Region,
-    loop_body: Region,
-    outputs: []Region,
+    tail_cond: Id,
+    loop_body: Id,
 };
 
 // inter procedural
 
+// Lambda node represents a function, the fucntion can get 0 or more arguments.
+// The way those arguments are implemented in the graph is just like every other expression.
+// That is to say, the argument should point to some eclass that represents an expression.
+// In the egraph, the arguments will actually be represented as the childrens of the node,
+// in oppose to the so called "logical" way to represent such a thing, the reason for this
+// decision is the fact that egraph are directed with the opposite way to the flow of consumption
+// lets suppose we have an add instruction, then its children will be its arguments, and not its outputs.
 const LambdaNode = struct {
-    arguments: []Type,
-    function_body: Region,
+    arguments: []Id,
+    function_body: Id,
 };
 
 const DeltaNode = struct {
@@ -53,8 +59,7 @@ const OmegaNode = struct {
 // that is to say, no instruction-order related optimization will be performed on
 // one block before and one block after this barrier
 const OptBarrier = struct {
-    inputs: []*Region,
-    output: []*Region,
+    code: Id,
 };
 
 const Node = union {
